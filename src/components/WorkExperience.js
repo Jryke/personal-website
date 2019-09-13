550bca3a0e68a62b39d69e511f64fc891233bd9c
Jryke <rykerson@gmail.com>
2019-09-13 23:09:21 +0700

    job details  updated

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -4,12 +4,41 @@ const WorkExperience = () => {
     return (
         <div id="experience" className="ui center aligned grid container" style={{padding: '3em'}}>
             <h2>Professional Work Experience</h2>
-            <h3 className="row">Professor of English - Boston Language Institute</h3>
-            <p>Description of duties here</p>
-            <h3 className="row">Senior Sales Consultant - iPayment Inc.</h3>
-            <p>Description of duties here</p>
-            <h3 className="row">Professor of English and Team Leader - Universidad Del Norte</h3>
-            <p>Description of duties here</p>
+            <h3 className="row">
+							Professor of English - Boston Language Institute
+							<br />
+							2017 - 2019
+						</h3>
+            <span className="row">Duties:</span>
+						<ul>
+							<li>Teaching and training of English For Foreign Born Professionals</li>
+							<li>Teacher of Intermediate, High-Intermediate and Conversational English for English language learners</li>
+							<li>Development of curriculum, lesson plans, and classroom activities</li>
+						</ul>
+            <h3 className="row">
+							Senior Sales Consultant - iPayment Inc.
+							<br />
+							2015 - 2018
+						</h3>
+						<span className="row">Duties:</span>
+						<ul>
+							<li>Sales of accounts and SaaS (primarily by phone and email)</li>
+							<li>Account representative for the lifetime of the account – maintenance (management) and customer service for accounts</li>
+							<li>Selling/working for 3 different brands within company as an English and Spanish representative</li>
+							<li>Experience in sales and extensive knowledge of credit card processing industry</li>
+							<li>One of the top producers in sales and revenue for 3 years.</li>
+						</ul>
+            <h3 className="row">
+							Professor of English and Team Leader - Universidad Del Norte
+							<br />
+							2011 - 2014
+						</h3>
+            <span className="row">Duties:</span>
+						<ul>
+							<li>Taught English to over 100 University students each semester</li>
+							<li>Led groups of teachers in several English language levels</li>
+							<li>Created assessments/exams for use in classes</li>
+						</ul>
         </div>
     )
 }
