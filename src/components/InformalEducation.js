d9cfe9072a0597b40bd7b6acfba339f942338cf7
Jryke <rykerson@gmail.com>
2019-09-13 23:55:44 +0700

    Tortuga Coders added

--- src/components/InformalEducation.js
+++ src/components/InformalEducation.js
@@ -1,32 +1,38 @@
 import React from 'react';
 
 const InformalEducation = () => {
-    return (
-        <div className="ui center aligned grid container" style={{padding: '3em'}}>
-            <h2 className="row">Informal Education</h2>
-            <div className="ui list">
-                <div className="item">
-                    <h3>Harvard Extension School:</h3>
-                    <p>Web Programming/JavaScript Course <br />
-                    Grade: A</p>
-                </div>
-                <br />
-                <br />
-                <div className="item">
-                    <h3>Udemy:</h3>
-                    <p>Modern JavaScript Bootcamp (2019) <br />
-                    Modern React With Redux</p>
-                </div>
-                <br />
-                <br />
-                <div className="item">
-                    <h3>FreeCodeCamp.org:</h3>
-                    <p>Responsive Web Design Certification <br />
-                    JavaScript Algorithms and Data Structures Certification</p>
-                </div>
-            </div>
+  return (
+    <div className="ui center aligned grid container" style={{padding: '3em'}}>
+      <h2 className="row">Informal Education</h2>
+      <div className="ui list">
+        <div className="item">
+          <h3>Harvard Extension School:</h3>
+          <p>Web Programming/JavaScript Course <br />
+          Grade: A</p>
         </div>
-    )
+        <br />
+        <br />
+				<div className="item">
+					<h3>Tortuga Coders</h3>
+					<p>9-week full-stack JavaScript web development bootcamp</p>
+				</div>
+				<br />
+				<br />
+        <div className="item">
+          <h3>Udemy:</h3>
+          <p>Modern JavaScript Bootcamp (2019) <br />
+          Modern React With Redux</p>
+        </div>
+        <br />
+        <br />
+        <div className="item">
+          <h3>FreeCodeCamp.org:</h3>
+          <p>Responsive Web Design Certification <br />
+          JavaScript Algorithms and Data Structures Certification</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default InformalEducation
