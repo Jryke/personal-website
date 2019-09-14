5fc41c76a7cccb988bfd79560f044a90411b9ec5
Jryke <rykerson@gmail.com>
2019-09-14 22:19:20 +0700

    content finished, styles to accomodate

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,60 +1,64 @@
 import React from "react";
+// import '../styles/bio.css'
 
 const Bio = () => {
   return (
-	  <div id="bio" className="ui center aligned grid container" style={{padding: '3em'}}>
-      <h2 className="row">Bio:</h2>
-			<p>Some general information about me.</p>
-			<div className="row">
+	  <div id='bio' className='center section'>
+      <h2>Bio:</h2>
+			<div>
+				<h3>About me:</h3>
+				<p>
+					I am a social and outgoing person but also enjoy quiet time to myself.  I like a good conversation but I am also content all alone with my computer or a book.  I’m always looking for a good project to work on because I get the most satisfaction out of producing and being creative.  Simply put, I like to learn things and then use what I learn to make things.
+				</p>
+			</div>
+			<div>
 				<h3>Professional and personal philosophies:</h3>
 			</div>
-			<div className="row">
+			<div>
 				<h4>work smarter... and harder</h4>
 				<p>
 					I consider myself to be intelligent, but I also know that intelligence is useless without applying it.  In my professional life (as well as anything that I do), I am a hard worker and I always strive to give my 100% in everything that I do.  I hold to the proverb "any job worth doing is a job worth doing well".  If I am going to put my time and energy into something, I am going to see it through properly; otherwise I am just wasting my time and energy.  Therfore, I take pride in everything I do and I do everything to the best of my ability.
 				</p>
 			</div>
-			<div className="row">
+			<div>
 				<h4>Create and be creative:</h4>
 				<p>
 					Growing up, my father was a carpenter and an architect so we had a woodshop in the basement of our house.  I spent a lot of my childhood in the basement making things (including my own toys or boxes to store my toys).  Among other expriences, I believe that this largely shaped my desire to be creative and build things.  I get a lot of satisfaction out of planning a project, executing it and seeing the desired result in the end.  This has played a big part for me becoming a web developer.  I really enjoy the creative process of building web sites and web apps from the beginning to the end.  I read a quote recently that really got my attention: "Be a creator, not a consumer".  Every day people are consuming more information and entertainment online; I want to be a part of creating it, not only consuming it.
 				</p>
 			</div>
-			<div className="row">
+			<div>
 				<h4>Be a lifelong learner:</h4>
 				<p>
 					I love to learn and I know that learning is the best use of my time.  I believe that if I am not learning (and enjoying what I am learning), I am wasting my time.  While studying my masters degree in Applied Linguistics to become a teacher, I learned not just how to be a good teacher but also to be a good student.
 				</p>
 			</div>
-			<div className="row">
+			<div>
 				<h4>Do what you love and love what you do:</h4>
 				<p>
-					Time is the most valuable thing that we have and we need to take advantage of it as much as possible to be happy.  To take advantage of our time, it is important to work doing something that you love to do, otherwise you are wasting your time doing things that don't contribute to your happiness.  Many people work to make money but don't have the time to enjoy it, this is counter-intuitive.  Enjoying what you do is the key a "successful" lifestyle, not just making money (although money is nice to have too!).
+					Time is the most valuable thing that we have and we need to take advantage of it as much as possible to be happy.  To take advantage of our time, it is important to work doing something that you love to do, otherwise you are wasting your time doing things that don't contribute to your happiness.  Many people work to make money but don't have the time to enjoy it, this is counter-intuitive.  Enjoying what you do is the key a &quot;successful&quot; lifestyle, not just making money (although money is nice to have too).
 				</p>
 			</div>
       <br />
-      <h3 className="row">Personal Interests:</h3>
-			<p>My interests vary...</p>
-			<div className="row">
+      <h3>Personal Interests:</h3>
+			<div>
 				<h4>Travel</h4>
-				<p>Travelling is my </p>
-				<h4>Music</h4>
-				<p>I love listening to music.  I am a music collector.  My love for music goes hand-in-hand with my love for travelling.  I have an extensive collection of music that I grew up with and music that I have acquired while travelling.  I play guitar and DJ.  While living in Colombia for 4 years, I became immersed in the local Champeta music, which is a Colombian/Afro-Caribbean music that is hard not to like.</p>
-				<h4>Languages</h4>
-				<p>I grew up speaking English and after finishing my undergraduate degree, I decided it was time to learn Spanish.  I am fluent in Spanish and I have learned some Portuguese.  I have since learned JavaScript which although not a human language, was still learning another language.  While learning JavaScript, I found that computer languages are more similar to human languages than I had thought.</p>
-				<br />
-				<p>History, Geography, Science (interesting topics/news articles?)<br />
-				Favorite tv shows, movies or books (music! - Reggae, Champeta) (Peter Pan - lessons I have learned)
-				<br />
-				Family life
-				<br />
-				Funny details about habits or interests / What people like about me
-				<br />
-				Unique personal history - living in Colombia, and other "interesting" things that I have done
+				<p>
+					I love to travel because I love to see and experience new things.  The world has so much beauty and I want to see all of it.  Travelling also allows me to experience other cultures and ways of thinking.  I often learn new customs while travelling that I can adapt to my own lifestyle.  I believe that this helps me improve who I am as a person and always continue becoming a better person.
+					<br />
+					<br />
+					Another aspect that drives me to travel is that I never know what I will see or experience next.  Travelling excites me because of the unknown aspect of going to a new place.  It&#39;s nice to see something that is beautiful, but to be surprised by it is even more delightful.
 				</p>
-				<br />
-				<h3 className="row">Hobbies:</h3>
-				<p>DJing, reading, learning</p>
+				<h4>Music</h4>
+				<p>
+					I love listening to music.  I am a music collector.  I have an extensive collection of music that I grew up with and music, music that fellow music addicts have told me about and music that I have acquired while travelling.  My love for music goes hand-in-hand with my love for travelling.  While living in Colombia for 4 years, I was fascinated by the abundance of music and rhythms that come from Colombia.  I became especially immersed in the local Champeta music, which is a Colombian/Afro-Caribbean music that is hard not to like.
+					<br />
+					<br />
+					In addition to listening to and collecting music, I play guitar and I am a DJ.  Like the chicken and the egg, I&#39;m not sure which came first, being a DJ or a music collector.  Either way, the two obviously compliment each other.  My favorite music to DJ is Afro-Caribbean music (Reggae, Soca, Champeta, etc.).  The songs that I play on guitar are mostly by Red Hot Chili Peppers and Jimi Hendrix.
+				</p>
+				<h4>Languages</h4>
+				<p>
+					I grew up speaking English and after finishing my undergraduate degree, I decided that I wanted to take some time to really learn Spanish.  I was determined to not just learn Spanish but master it.  After learning some Spanish, my interest in language let me to teach English and get a masters degree in Applied Linguistics at UMass Boston.  Once I completed my masters degree, I lived in Barranquilla, Colombia for four years teaching English and polishing my Spanish.  While living there, I learned Spanish and Coste&#241;ol (caribbean style Spanish).  Learning Spanish as well as I did gave me supreme confidence that I can learn anything as long as I put the time and effort forward.  I am now fluent in Spanish and I have studied Portuguese.  Most recently I have learned JavaScript which although not a human language, I still consider learning another language.  While learning JavaScript, I found that computer languages are more similar to human languages than I had thought.  All languages, human or computer, fascinate me.
+				</p>
 			</div>
 
 	  </div>
