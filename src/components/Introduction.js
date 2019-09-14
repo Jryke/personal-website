8470eab6cf37b6349e3151daf69323a108843dd6
Jryke <rykerson@gmail.com>
2019-09-14 16:55:42 +0700

    intro stylesheet added

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import '../styles/introduction.css'
 
 const Introduction = () => {
     return (
-        <div className="ui center aligned grid container">
-            <div className="row" style={{ padding: '3em' }}>
+        <div className='center intro'>
                 <h1>Jesse Rykerson</h1>
-            </div>
-            <div className="row">
                 <h2>Web/App Developer</h2>
-            </div>
         </div>
     );
 };
