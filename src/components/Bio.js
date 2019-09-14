e732731da0b0e16e050dc4072286c22f4807b4f1
Jryke <rykerson@gmail.com>
2019-09-14 16:58:27 +0700

    text added

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,6 +4,7 @@ const Bio = () => {
   return (
 	  <div id="bio" className="ui center aligned grid container" style={{padding: '3em'}}>
       <h2 className="row">Bio:</h2>
+			<p>Some general information about me.</p>
 			<div className="row">
 				<h3>Professional and personal philosophies:</h3>
 			</div>
@@ -16,7 +17,7 @@ const Bio = () => {
 			<div className="row">
 				<h4>Create and be creative:</h4>
 				<p>
-					Growing up, my father was a carpenter and an architect so we had a woodshop in the basement of our house.  I spent a lot of my childhood in the basement making things (including my own toys or boxes to store my toys).  Among other expriences, I believe that this largely shaped my desire to be creative and build things.  I get a lot of satisfaction out of planning a project, executing it and seeing the desired result at the end.  This has played a big part for me becoming a web developer.  I really enjoy the creative process from beginning to end of building web sites and web apps.
+					Growing up, my father was a carpenter and an architect so we had a woodshop in the basement of our house.  I spent a lot of my childhood in the basement making things (including my own toys or boxes to store my toys).  Among other expriences, I believe that this largely shaped my desire to be creative and build things.  I get a lot of satisfaction out of planning a project, executing it and seeing the desired result in the end.  This has played a big part for me becoming a web developer.  I really enjoy the creative process of building web sites and web apps from the beginning to the end.  I read a quote recently that really got my attention: "Be a creator, not a consumer".  Every day people are consuming more information and entertainment online; I want to be a part of creating it, not only consuming it.
 				</p>
 			</div>
 			<div className="row">
@@ -32,16 +33,30 @@ const Bio = () => {
 				</p>
 			</div>
       <br />
-      <h2 className="row">Personal Interests:</h2>
-      <p >Music/DJing, Traveling, Languages, History, Geography, Science (interesting topics/news articles?)<br />
-			Favorite tv shows, movies or books (music! - Reggae, Champeta) (Peter Pan - lessons I have learned)
-			<br />
-			Family life
-			<br />
-			Funny details about habits or interests / What people like about me
-			<br />
-			Unique personal history - living in Colombia, and other "interesting" things that I have done
-			</p>
+      <h3 className="row">Personal Interests:</h3>
+			<p>My interests vary...</p>
+			<div className="row">
+				<h4>Travel</h4>
+				<p>Travelling is my </p>
+				<h4>Music</h4>
+				<p>I love listening to music.  I am a music collector.  My love for music goes hand-in-hand with my love for travelling.  I have an extensive collection of music that I grew up with and music that I have acquired while travelling.  I play guitar and DJ.  While living in Colombia for 4 years, I became immersed in the local Champeta music, which is a Colombian/Afro-Caribbean music that is hard not to like.</p>
+				<h4>Languages</h4>
+				<p>I grew up speaking English and after finishing my undergraduate degree, I decided it was time to learn Spanish.  I am fluent in Spanish and I have learned some Portuguese.  I have since learned JavaScript which although not a human language, was still learning another language.  While learning JavaScript, I found that computer languages are more similar to human languages than I had thought.</p>
+				<br />
+				<p>History, Geography, Science (interesting topics/news articles?)<br />
+				Favorite tv shows, movies or books (music! - Reggae, Champeta) (Peter Pan - lessons I have learned)
+				<br />
+				Family life
+				<br />
+				Funny details about habits or interests / What people like about me
+				<br />
+				Unique personal history - living in Colombia, and other "interesting" things that I have done
+				</p>
+				<br />
+				<h3 className="row">Hobbies:</h3>
+				<p>DJing, reading, learning</p>
+			</div>
+
 	  </div>
   )
 }
