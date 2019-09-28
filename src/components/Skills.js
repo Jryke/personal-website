9cda34a308243abb5a266987b05e3aa3d68dc549
Jryke <rykerson@gmail.com>
2019-09-28 14:51:21 +0700

    basic layout completed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,49 +6,76 @@ const Skills = () => {
   return (
 		<>
 			<Navbar />
-			<div id="skills" className="center section">
-				<h2>Skills:</h2>
-				<div className="list">
-					<h3>Technologies and Libraries:</h3>
-					<h4>Front-end:</h4>
-					<div className='listWrapper'>
-						<ul>
-							<li>HTML5</li>
-							<li>CSS3</li>
-							<li>JavaScript ECMAScript 2019</li>
-							<li>React.js</li>
-							<li>Redux</li>
-							<li>Bootstrap</li>
-							<li>Semantic UI</li>
-							<li>jQuery</li>
-							<li>Sass</li>
-						</ul>
+			<div className='skills-header-background'>
+				<div className='skills-header-background-overlay'>
+					<div className='skills-header-text-container'>
+						<h1 className='skills-header-text'>skills</h1>
 					</div>
+				</div>
+			</div>
+			<div className='center skills section background-grey'>
+				<h1>Technologies and Libraries:</h1>
+				<div className='grid-row-container'>
+					<div className='grid-row'>
+						<div id="skills">
+							<h2>Front-end:</h2>
+							<div className="list">
+								<div className='listWrapper'>
+									<div>
+										<ul>
+											<li>HTML5</li>
+											<li>CSS3</li>
+											<li>JavaScript ECMAScript 2019</li>
+											<li>React.js</li>
+											<li>Redux</li>
+											<li>Bootstrap</li>
+											<li>Semantic UI</li>
+											<li>jQuery</li>
+											<li>Sass</li>
+										</ul>
+									</div>
+									<div>
+										<h2>Back-end:</h2>
+										<ul>
+											<li>Node</li>
+											<li>Express</li>
+											<li>MongoDB</li>
+										</ul>
+									</div>
+									<div>
+										<h2>Other applications:</h2>
+										<ul>
+											<li>MS Office Suite</li>
+											<li>Adobe Photoshop</li>
+										</ul>
+									</div>
+									<div>
+										<h3>Other Skills:</h3>
+										<ul>
+											<li>Bilingual: fluent in English and Spanish</li>
+											<li>Creative, self-motivated, goal oriented and an avid learner</li>
+											<li>Effective team member and team leader</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className='skills-picture'>
+							<div style={{backgroundImage:`url('/images/20181221_123658.jpg')`, backgroundSize:'cover', }}></div>
+						</div>
+					</div>
+				</div>
 
-					<h4>Back-end:</h4>
-					<ul>
-						<li>Node</li>
-						<li>Express</li>
-						<li>MongoDB</li>
-					</ul>
 
-					<h4>Other applications:</h4>
-					<ul>
-						<li>MS Office Suite</li>
-						<li>Adobe Photoshop</li>
-					</ul>
+
+
+
+
+
 
 				</div>
 				<br />
-				<div>
-					<h3>Other Skills:</h3>
-					<ul>
-						<li>Bilingual: fluent in English and Spanish</li>
-						<li>Creative, self-motivated, goal oriented and an avid learner</li>
-						<li>Effective team member and team leader</li>
-					</ul>
-				</div>
-			</div>
+
 		</>
   )
 }
