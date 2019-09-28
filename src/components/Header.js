e32004f2aa65d6d5717ab240425850163c68b581
Jryke <rykerson@gmail.com>
2019-09-28 19:11:23 +0700

    Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Header extends React.Component {
+	render() {
+		return(
+			<>
+			<div className={`${this.props.componentName}-header-background`}>
+				<div className={`${this.props.componentName}-header-background-overlay`}>
+					<div className={`${this.props.componentName}-header-text-container`}>
+						<h1 className={`${this.props.componentName}-header-text`}>{this.props.componentName}</h1>
+					</div>
+				</div>
+			</div>
+			</>
+		)
+	}
+}
+
+export default Header
