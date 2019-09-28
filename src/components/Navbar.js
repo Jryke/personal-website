75f1675625874c0c60d826b2a0c13ab0d60a7db2
Jryke <rykerson@gmail.com>
2019-09-28 19:12:31 +0700

    link order rearranged

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,12 +14,12 @@ const Navbar = () => {
 				*/}
 				<span className="nav-name"><Link to="/">Jesse Rykerson</Link></span>
 					<ul className='nav'>
-						<li className='nav-link'><Link to='/about'>About</Link></li>
+						<li className='nav-link'><Link to='./portfolio'>Portfolio</Link></li>
 						<li className='nav-link'><Link to='/skills'>Skills</Link></li>
 						<li className='nav-link'><Link to='./experience'>Experience</Link></li>
 						<li className='nav-link'><Link to='./education'>Education</Link></li>
-						<li className='nav-link'><Link to='./portfolio'>Portfolio</Link></li>
 						<li className='nav-link'><a href='#'>Resume</a></li>
+						<li className='nav-link'><Link to='/about'>About</Link></li>
 						<li className='nav-link'><a href='#'>Contact</a></li>
 					</ul>
 			</nav>
