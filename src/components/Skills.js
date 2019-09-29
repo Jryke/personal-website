61fccf8bbeef70319683dcf7677a894a83908df0
Jryke <rykerson@gmail.com>
2019-09-29 18:52:35 +0700

    skills text layout completed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,56 +9,53 @@ const Skills = () => {
 		<>
 			<Navbar />
 			<Header componentName={'skills'} text={'skills'} />
-			<div className='center skills section background-grey'>
-				<h1>Technologies and Libraries:</h1>
+			<div className='skills background-grey'>
+				<h1 className='skills-body-header'>Technologies and Libraries:</h1>
 				<div className='grid-row-container'>
 					<div className='grid-row'>
-						<div id="skills">
-							<h2>Front-end:</h2>
-							<div className="list">
-								<div className='listWrapper'>
-									<div>
-										<ul>
-											<li>HTML5</li>
-											<li>CSS3</li>
-											<li>JavaScript ECMAScript 2019</li>
-											<li>React.js</li>
-											<li>Redux</li>
-											<li>Bootstrap</li>
-											<li>Semantic UI</li>
-											<li>jQuery</li>
-											<li>Sass</li>
-										</ul>
-									</div>
-									<div>
-										<h2>Back-end:</h2>
-										<ul>
-											<li>Node</li>
-											<li>Express</li>
-											<li>MongoDB</li>
-										</ul>
-									</div>
-									<div>
-										<h2>Other applications:</h2>
-										<ul>
-											<li>MS Office Suite</li>
-											<li>Adobe Photoshop</li>
-										</ul>
-									</div>
-									<div>
-										<h3>Other Skills:</h3>
-										<ul>
-											<li>Bilingual: fluent in English and Spanish</li>
-											<li>Creative, self-motivated, goal oriented and an avid learner</li>
-											<li>Effective team member and team leader</li>
-										</ul>
-									</div>
-								</div>
+						<div className='skills-list-wrapper'>
+							<div className='skill-info'>
+								<h2 className='skills-title'>Front-end</h2>
+								<ul className='skills-list'>
+									<li>HTML5</li>
+									<li>CSS3</li>
+									<li>JavaScript ECMAScript 2019</li>
+									<li>React.js</li>
+									<li>Redux</li>
+									<li>Ionic</li>
+									<li>Bootstrap</li>
+									<li>Semantic UI</li>
+									<li>jQuery</li>
+									<li>Sass</li>
+								</ul>
+							</div>
+							<div className='skill-info'>
+								<h2 className='skills-title'>Back-end</h2>
+								<ul className='skills-list'>
+									<li>Node</li>
+									<li>Express</li>
+									<li>MongoDB</li>
+									<li>SQL</li>
+								</ul>
+							</div>
+							<div className='skill-info'>
+								<h2 className='skills-title'>Applications</h2>
+								<ul className='skills-list'>
+									<li>MS Office Suite</li>
+									<li>Adobe Photoshop</li>
+									<li>Salesforce</li>
+								</ul>
+							</div>
+							<div className='skill-info'>
+								<h2 className='skills-title'>Other Skills</h2>
+								<ul className='skills-list'>
+									<li>Bilingual: fluent in English and Spanish</li>
+									<li>Creative, self-motivated, goal oriented and an avid learner</li>
+									<li>Effective team member and team leader</li>
+								</ul>
 							</div>
 						</div>
-						<div className='skills-picture'>
-							<div style={{backgroundImage:`url('/images/20181221_123658.jpg')`, backgroundSize:'cover', }}></div>
-						</div>
+						<div className='skills-picture'></div>
 					</div>
 				</div>
 			</div>
