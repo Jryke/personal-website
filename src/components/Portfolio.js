f4cb5954eecbb26360f5a46dfa6da065b405cf16
Jryke <rykerson@gmail.com>
2019-09-30 13:04:02 +0700

    body styled

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,24 +9,26 @@ const Portfolio = () => {
 	<>
 		<Navbar />
 		<Header componentName={'portfolio'} text={'portfolio'} />
-		<div className="center portfolio section background-grey">
-			<h1>Portfolio:</h1>
-			<div className='grid-row-container'>
-				<div className='grid-row'>
-					<div>
-						<h2>Professional Projects:</h2>
-						<a href="https://www.newenglandlanguage.com/" target="_blank" rel="noopener noreferrer">newenglandlanguage.com</a>
-					</div>
-					<div>
-						<h2>Public accounts:</h2>
-						<ul>
-							<li><a href="https://www.linkedin.com/in/jesse-rykerson/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-							<li><a href="https://github.com/jryke" target="_blank" rel="noopener noreferrer">Github</a></li>
-							<li><a href="https://codepen.io/Jryke" target="_blank" rel="noopener noreferrer">Codepen</a></li>
-							<li><a href="https://www.freecodecamp.org/jryke" target="_blank" rel="noopener noreferrer">FreeCodeCamp</a></li>
-						</ul>
-					</div>
+		<div className='portfolio background-grey'>
+			<div className='portfolio-grid'>
+				<h1 className='body-header right'>Professional Projects:</h1>
+				<div></div>
+				<div className='project-info right'>
+					<p>New England Language School Website</p>
+					<a href="https://www.newenglandlanguage.com/" target='_blank' rel='noopener noreferrer'>{`https://www.newenglandlanguage.com/`}</a>
 				</div>
+				<div className='portfolio-img-container'>
+					<div className='nels-img'></div>
+				</div>
+				
+				<h1 className='body-header section right'>Public accounts:</h1>
+				<div></div>
+				<ul className='right'>
+					<li><a href="https://www.linkedin.com/in/jesse-rykerson/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+					<li><a href="https://github.com/jryke" target="_blank" rel="noopener noreferrer">Github</a></li>
+					<li><a href="https://codepen.io/Jryke" target="_blank" rel="noopener noreferrer">Codepen</a></li>
+					<li><a href="https://www.freecodecamp.org/jryke" target="_blank" rel="noopener noreferrer">FreeCodeCamp</a></li>
+				</ul>
 			</div>
 		</div>
 		<Footer />
