fa2eed50cf8f4dd2450ee429e24f59045bcc7411
Jryke <rykerson@gmail.com>
2019-10-01 20:31:27 +0700

    ScrollToTop added for navigation

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop'
 import Introduction from './Introduction'
 import About from './About'
 import Skills from './Skills'
@@ -12,15 +13,17 @@ class Routes extends React.Component {
 	render() {
 		return(
 			<BrowserRouter>
-				<Switch>
-					<Route path='/about' component={About} />
-					<Route path='/skills' component={Skills} />
-					<Route path='/experience' component={WorkExperience} />
-					<Route path='/education' component={Education} />
-					<Route path='/portfolio' component={Portfolio} />
-					<Route path='/App' component={App} />
-					<Route path='/' component={Introduction} />
-				</Switch>
+				<ScrollToTop>
+					<Switch>
+						<Route path='/about' component={About} />
+						<Route path='/skills' component={Skills} />
+						<Route path='/experience' component={WorkExperience} />
+						<Route path='/education' component={Education} />
+						<Route path='/portfolio' component={Portfolio} />
+						<Route path='/App' component={App} />
+						<Route path='/' component={Introduction} />
+					</Switch>
+				</ScrollToTop>
 			</BrowserRouter>
 		)
 	}
