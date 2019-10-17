94d1d26800279b06162ecf413b5c99f966bf9bc7
Jryke <rykerson@gmail.com>
2019-10-17 11:11:42 -0400

    scroll down click working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class Header extends React.Component {
+	clickScroll = () => {
+		window.scrollTo({
+			top: window.innerHeight,
+			behavior: 'smooth'
+		})
+	}
 	render() {
 		return(
 			<>
@@ -9,10 +15,10 @@ class Header extends React.Component {
 					<div className={`${this.props.componentName}-header-text-container`}>
 						<h1 className={`${this.props.componentName}-header-text`}>{this.props.text}</h1>
 					</div>
-					<div className='scroll-down-container'>
+					<div className='scroll-down-container' onClick={this.clickScroll}>
 						<small className='scroll-down'>scroll down</small>
 						<br />
-						<i class="fas fa-angle-down"></i>
+						<i className="fas fa-angle-down"></i>
 					</div>
 				</div>
 			</div>
